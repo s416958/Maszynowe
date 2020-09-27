992d0abc91cedfa627a2353216c4fe18eb751753
s416958 <35811807+s416958@users.noreply.github.com>
2020-09-27 12:45:50 +0200

    Update exercise_1_part_1.py

--- exercise_1_part_1.py
+++ exercise_1_part_1.py
@@ -1,30 +1,31 @@
 import pandas as pd
 
-#1
-#Data loading (train.tsv) as DataFrame (df_data)
+# 1
+# Data loading (train.tsv) as DataFrame (df_data)
 df_data = pd.read_csv(
         'train.tsv',
         sep='\t',
-        names=['price', 'nr_rooms', 'meters', 'floors', 'location', 'description'])
+        names=['price', 'nr_rooms', 'meters', 'floors', 'location', 
+               'description'])
 
-#Counting mean of apartments price and round
+# Counting mean of apartments price and round
 mean_price = round(df_data['price'].mean(), 3)
 
-#Saving mean_price in out0.csv file
+# Saving mean_price in out0.csv file
 with open('out0.csv', 'w') as file:
     file.write(str(mean_price))
 
-#2
-#Counting price for square meter and round
+# 2
+# Counting price for square meter and round
 df_data['price_meters'] = (df_data['price']/df_data['meters']).round(3)
 
-#Choosing data with number of rooms (=>3), price and
-#price for square meter (lower than mean price for square meter)
+# Choosing data with number of rooms (=>3), price and
+# price for square meter (lower than mean price for square meter)
 df_select_data = df_data.loc[
                 df_data['nr_rooms'] >= 3 &
                 (df_data['price_meters'] <
                  round(float(df_data['price_meters'].mean()), 2))][
                  ['nr_rooms', 'price', 'price_meters']]
 
-#Saving results in out1.csv file
+# Saving results in out1.csv file
 df_select_data.to_csv('out1.csv', header=False, index=None)
